1141bac4561ceefb6cf97e0342ddc216c46ef38d
Amogh Vishwakarma <av670@cam.ac.uk>
2026-01-24 00:48:41 +0000

    made changes to init of hmm and ran experiments

---
+++ skeleton_code/output/hmm-sem-xpos-dir-alpha-0.8/executable.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "This job is running on: $(hostname)"
+
+cd /home/av670/rds/hpc-work/NLP-assignment-3/skeleton_code
+
+echo "Present working directory: $(pwd)"
+
+echo "Job ID: $SLURM_JOB_ID"
+echo "Job start time: $(date)"
+
+module purge
+
+echo "activating venv"
+source ../venv/bin/activate
+
+echo "running file"
+
+python3.12 -u -m main train-test hmm-sEM xpos --max-epochs 20 1 --save-path ./output/hmm-sem-xpos-dir-alpha-0.8/save/hmm-sem-xpos-dir-alpha-0.8.pt --res-path ./output/hmm-sem-xpos-dir-alpha-0.8/results/hmm-sem-xpos-dir-alpha-0.8.csv --alpha-sem 0.8
+
+echo "finished job"
